9f87499f6753afdb2d9dc8d91917b74608a1536e
shuhei <hello@shuhei.world>
2023-04-07 21:15:51 +0900

    add metadata type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import 'src/app/globals.css';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'My Nextjs template',
 };
 
@@ -11,7 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ja'>
-      <head />
       <body>{children}</body>
     </html>
   );
